9e5cbd5749f580da15f190cd1e8e7d4c9f695349
DanielNabil74 <danynabil74@gmail.com>
2023-11-27 15:03:05 +0200

    Rawan

---
+++ database/postgres.js
@@ -0,0 +1,9 @@
+const pg = require ("pg")
+const pool = new pg.pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+module.exports = pool;
